311de500f92f8a298784f964a1b967cf5d97fca2
Craig Booth <craigb@neller.com.au>
2020-04-12 11:58:30 +0930

    Initial verion

---
+++ Booth.EventStore/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleToAttribute("Booth.EventStore.Test")]
+
+namespace Booth.EventStore
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+    }
+}
